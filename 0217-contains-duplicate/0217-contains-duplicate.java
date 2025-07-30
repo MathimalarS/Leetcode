56857d2bfb738d835f9d5bc79065f5c2df4d0b21
Mathimalar <125265563+MathimalarS@users.noreply.github.com>
2025-07-30 13:41:42 +0530

    Time: 14 ms (61.35%), Space: 58.4 MB (53.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,12 @@
-// import java.util.HashSet;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        int n = nums.length;
-        // Optimization: Pre-size the HashSet to avoid rehashing
-        HashSet<Integer> set = new HashSet<>(n);
-        for (int num : nums)
-            if (!set.add(num)) return true;
+    Map<Integer,Boolean> map = new HashMap<>();
+    for (int num : nums) {
+        if (map.containsKey(num)) {
+          return true;
+        }
+        map.put(num,true);
+    }
         return false;
     }
 }
