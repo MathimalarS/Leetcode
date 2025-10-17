5b010dfb990308de609628e9f159ee68b6d4660c
Mathimalar <125265563+MathimalarS@users.noreply.github.com>
2025-10-17 21:59:55 +0530

    Time: 4 ms (90.48%), Space: 58.4 MB (48.09%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -3,13 +3,11 @@ class Solution {
         int slow = nums[0];
         int fast = nums[0];
 
-        // Detect intersection
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
         } while (slow != fast);
 
-        // Find entrance (duplicate)
         slow = nums[0];
         while (slow != fast) {
             slow = nums[slow];
